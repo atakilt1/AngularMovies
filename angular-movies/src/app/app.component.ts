bc9be08893ed615373294ce8223233d1afb0488c
atakilt1 <atakilt.send@gmail.com>
2026-06-21 12:44:43 +0300

    Added paramaters to movies list

--- angular-movies/src/app/app.component.ts
+++ angular-movies/src/app/app.component.ts
@@ -4,9 +4,40 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: false,
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent  { 
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+
+    this.moviesInTheaters = [
+      {
+        title: 'Spider Man',
+        releaseDate: new Date(),
+        price: 1400.99,
+      },
+      {
+        title: 'Moana',
+        releaseDate: new Date('2015-05-02'),
+        price: 300.99,
+      },
+    ];
+
+    this.moviesInReleases =  [
+      {
+        title: 'Avengers',
+        releaseDate: new Date(),
+        price: 500.99,
+      },
+      {
+        title: 'Toy Story',
+        releaseDate: new Date('2020-05-02'),
+        price: 600.99,
+      },
+    ];
+  }
+
+  moviesInTheaters: any;
+  moviesInReleases: any;
 
   title = 'Welcome to Tabor Movie Store';
 }
--- angular-movies/src/app/movies/movies-list/movies-list.component.ts
+++ angular-movies/src/app/movies/movies-list/movies-list.component.ts
@@ -1,27 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-movies-list',
   standalone: false,
   templateUrl: './movies-list.component.html',
-  styleUrl: './movies-list.component.css'
+  styleUrl: './movies-list.component.css',
 })
 export class MoviesListComponent implements OnInit {
-    ngOnInit(): void {
-    setTimeout(()=>{
-      this.movies = [{
-    title: "Spider Man",
-    releaseDate: new Date(),
-    price: 1400.99
-  },
-  {
-    title: "Moana",
-    releaseDate: new Date("2015-05-02"),
-    price: 300.99
-  },
-]
-    },1000)    
+  ngOnInit(): void {
   }
 
+  @Input()
   movies: any;
 }
